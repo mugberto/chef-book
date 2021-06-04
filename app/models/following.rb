aae47a8759dd0ee4e5a0c4cb6cdb9167f3b93026
Hubert MUGABO <mugberto@gmail.com>
2021-06-04 17:16:14 +0200

    Add association between user and following models

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,2 +1,4 @@
 class Following < ApplicationRecord
+  belongs_to :follower, classname: 'User', foreign_key: :follower_id
+  belongs_to :whom_he_follows, classname: 'User', foreign_key: :following_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :followers, classname: 'Following', foreign_key: :follower_id
+  has_many :people_he_follows, classname: 'Following', foreign_key: :following_id
 end
