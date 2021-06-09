28b9c13edb11b861f8a0fcb7d093e77a1645d208
Hubert MUGABO <mugberto@gmail.com>
2021-06-09 10:55:48 +0200

    Add style to the layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def msg_background(name)
+    if name == 'notice'
+      'alert alert-success mb-0 rounded-0'
+    elsif name == 'alert'
+      'alert alert-danger mb-0 rounded-0'
+    end
+  end
+
+  def log_in_menu
+    return '' unless current_user
+
+    content_tag(:span, "Hi #{current_user.fullname}!") + \
+      link_to('Log out', session_path(1), method: :delete, class: 'btn btn-secondary ms-3')
+  end
 end
