d3db0919d571f8546c3aa4f6c33208b5d3ff605d
Hubert MUGABO <mugberto@gmail.com>
2021-06-10 00:53:32 +0200

    Add profile edit page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  helper_method :user_authenticated?
 
   def current_user
     User.find(session[:user_id]) if session[:user_id]
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,10 @@ module UsersHelper
       button_to 'follow', follow_path, params: { followed_id: user.id }
     end
   end
+
+  def link_to_log_in_page
+    return '' if user_authenticated?
+
+    content_tag(:span, 'Already have an account?') + link_to('Log in', new_session_path)
+  end
 end
