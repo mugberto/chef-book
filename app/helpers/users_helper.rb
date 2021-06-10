2d267f783a00d23e9cb43a1d5a584974fb17911a
Hubert MUGABO <mugberto@gmail.com>
2021-06-10 04:12:00 +0200

    Add style to the UI

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,11 @@
 module UsersHelper
   def follow_btn(user)
     if user == current_user
-      button_to 'follow', follow_path, params: { followed_id: user.id }, disabled: true
+      button_to 'follow', follow_path, params: { followed_id: user.id }, disabled: true, class: 'btn btn-secondary'
     elsif current_user.followed_user? user
-      button_to 'unfollow', unfollow_path, params: { followed_id: user.id }, method: :delete
+      button_to 'unfollow', unfollow_path, params: { followed_id: user.id }, method: :delete, class: 'btn btn-success'
     else
-      button_to 'follow', follow_path, params: { followed_id: user.id }
+      button_to 'follow', follow_path, params: { followed_id: user.id }, class: 'btn btn-success'
     end
   end
 
